ac868b647b0eeab9a21f0d37bd714f68fb298533
KnightMiner <knightminer4@gmail.com>
2025-02-23 23:31:02 -0500

    Small EFLN cleanup
    While experimenting a bit with explosions, was refreshing myself on what makes the EFLN explosion odd; mostly just did some local variable renames to make the logic easier to track.
    One potential feature would want its own custom explosion logic, this serves as a good base for some of that logic
    New features include variable to set damage amount, along with a nice helper for syncing the explosion and updated to latest vanilla logic.

--- src/main/java/slimeknights/tconstruct/gadgets/entity/EFLNExplosion.java
+++ src/main/java/slimeknights/tconstruct/gadgets/entity/EFLNExplosion.java
@@ -1,6 +1,9 @@
 package slimeknights.tconstruct.gadgets.entity;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.protocol.game.ClientboundExplodePacket;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.util.Mth;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
@@ -9,7 +12,6 @@ import net.minecraft.world.entity.item.PrimedTnt;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.enchantment.ProtectionEnchantment;
 import net.minecraft.world.level.Explosion;
-import net.minecraft.world.level.ExplosionDamageCalculator;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.gameevent.GameEvent;
@@ -21,12 +23,20 @@ import net.minecraftforge.event.ForgeEventFactory;
 import javax.annotation.Nullable;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
-/** Custom explosion logic for EFLNs, more spherical and less random, plus works underwater */
+/**
+ * Custom explosion logic for EFLNs, more spherical and less random, plus works underwater and more control over damage amount.
+ * Loosely, the distinction between this and a regular explosion is this simply breaks all blocks within range if the power is high enough.
+ * A normal explosion does a bunch of ray casts from the center, and those ray casts can be interrupted by blocks with high enough resistance (such as water).
+ */
 public class EFLNExplosion extends Explosion {
-  public EFLNExplosion(Level world, @Nullable Entity entity, @Nullable DamageSource damage, @Nullable ExplosionDamageCalculator context, double x, double y, double z, float size, boolean causesFire, BlockInteraction mode) {
-    super(world, entity, damage, context, x, y, z, size, causesFire, mode);
+  /** Damage multiplier; setting to 7 will match the vanilla explosion. */
+  private final double damage;
+  public EFLNExplosion(Level world, @Nullable Entity entity, double damage, @Nullable DamageSource source, double x, double y, double z, float size, boolean causesFire, BlockInteraction mode) {
+    super(world, entity, source, null, x, y, z, size, causesFire, mode);
+    this.damage = damage;
   }
 
   @Override
@@ -51,18 +61,18 @@ public class EFLNExplosion extends Explosion {
             }
 
             // explosion "strength" at the current position
-            float f = this.radius * (1f - distance / (radius));
+            float strength = this.radius * (1f - distance / (radius));
             BlockState blockstate = this.level.getBlockState(blockpos);
 
             FluidState fluid = this.level.getFluidState(blockpos);
-            float f2 = Math.max(blockstate.getExplosionResistance(this.level, blockpos, this), fluid.getExplosionResistance(this.level, blockpos, this));
+            float power = Math.max(blockstate.getExplosionResistance(this.level, blockpos, this), fluid.getExplosionResistance(this.level, blockpos, this));
             if (this.source != null) {
-              f2 = this.source.getBlockExplosionResistance(this, this.level, blockpos, blockstate, fluid, f2);
+              power = this.source.getBlockExplosionResistance(this, this.level, blockpos, blockstate, fluid, power);
             }
 
-            f -= (f2 + 0.3F) * 0.3F;
+            strength -= (power + 0.3F) * 0.3F;
 
-            if (f > 0.0F && (this.source == null || this.source.shouldBlockExplode(this, this.level, blockpos, blockstate, f))) {
+            if (strength > 0.0F && (this.source == null || this.source.shouldBlockExplode(this, this.level, blockpos, blockstate, strength))) {
               set.add(blockpos);
             }
           }
@@ -98,21 +108,33 @@ public class EFLNExplosion extends Explosion {
         }
         if (length > 1.0E-4D) {
           double strength = (1.0D - distance) * getSeenPercent(center, entity);
-          entity.hurt(this.getDamageSource(), (int)((strength * strength + strength) / 2 * diameter + 1));
+          entity.hurt(this.getDamageSource(), (int)((strength * strength + strength) / 2 * damage * diameter + 1));
 
           // apply enchantment
-          // TODO 1.19.4, this was broke, reportably fixed in 1.19.4+
           double reducedStrength = strength;
           if (entity instanceof LivingEntity living) {
             reducedStrength = ProtectionEnchantment.getExplosionKnockbackAfterDampener(living, strength);
           }
-          entity.setDeltaMovement(entity.getDeltaMovement().add(dir.scale(reducedStrength / length)));
+          Vec3 velocity = dir.scale(reducedStrength / length);
+          entity.setDeltaMovement(entity.getDeltaMovement().add(velocity));
           if (entity instanceof Player player) {
             if (!player.isCreative() || !player.getAbilities().flying) {
-              // TODO 1.19.4: shouldn't this be reducedStrength? just copied vanilla here
-              this.getHitPlayers().put(player, dir.scale(strength / length));
+              this.getHitPlayers().put(player, velocity);
             }
           }
+        }
+      }
+    }
+  }
+
+  /** Syncs this explosion to the client */
+  public void syncToClient(Level level) {
+    if (!level.isClientSide && level instanceof ServerLevel server) {
+      List<BlockPos> toBlow = interactsWithBlocks() ? getToBlow() : List.of();
+      Map<Player,Vec3> hitPlayers = getHitPlayers();
+      for (ServerPlayer player : server.players()) {
+        if (player.distanceToSqr(getPosition()) < 4096.0D) {
+          player.connection.send(new ClientboundExplodePacket(x, y, z, radius, toBlow, hitPlayers.get(player)));
         }
       }
     }
